4ca6400c4ae90f6f84b3236c88dd19fd2b3a983e
Daniel Fitzgerald <djfitz@mit.edu>
2016-12-04 13:47:59 -0500

    headlight
    hardware doesn’t work

--- SerialCarControl/SerialCarControl.ino
+++ SerialCarControl/SerialCarControl.ino
@@ -14,7 +14,7 @@
 //int bluetoothTx = 2;  // TX-O pin of bluetooth mate, Arduino D2
 //int bluetoothRx = 3;  // RX-I pin of bluetooth mate, Arduino D3
 //SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
-
+void updateControl();
 Servo steering_servo_front;
 Servo steering_servo_back;
 void setupControls(){
@@ -26,6 +26,8 @@ void setupControls(){
   #ifdef BIDIRECTIONAL_DRIVE
     pinMode(MOTOR_DIRECTION_PIN, OUTPUT);
   #endif
+
+  updateControl();
 }
 
 SteerAngle steer_angle = 0;
@@ -42,7 +44,7 @@ void updateControl(){
     analogWrite(MOTOR_SPEED_PIN, map(drive_speed, 0, DRIVE_SPEED_MAX, 0, 255));
   }else{
     #ifdef BIDIRECTIONAL_DRIVE
-      digitalWrite(MOTOR_DIRECTION_PIN, FORWARD);
+      digitalWrite(MOTOR_DIRECTION_PIN, BACKWARD);
       analogWrite(MOTOR_SPEED_PIN, map(-drive_speed, 0, DRIVE_SPEED_MAX, 0, 255));
     #else
       digitalWrite(MOTOR_SPEED_PIN, LOW);
@@ -50,37 +52,59 @@ void updateControl(){
   }
 }
 
-void setupControlIndicators(){
-  pinMode(LEFT_TURN_LED_PIN, OUTPUT);
-  pinMode(RIGHT_TURN_LED_PIN, OUTPUT);
-  pinMode(FORWARDSPEED_LED_PIN, OUTPUT);
-  #ifdef BIDIRECTIONAL_DRIVE
-    pinMode(BACKWARDSPEED_LED_PIN, OUTPUT);
-  #endif
-  updateControlIndication();
-}
-void updateControlIndication(){
-  if (steer_angle >= 0){
-    analogWrite(RIGHT_TURN_LED_PIN, map(steer_angle, 0, STEERING_ANGLE_MAX, 0, 255));
-    digitalWrite(LEFT_TURN_LED_PIN, LOW);
-  }else{
-    analogWrite(LEFT_TURN_LED_PIN, map(-steer_angle, 0, STEERING_ANGLE_MAX, 0, 255));
-    digitalWrite(RIGHT_TURN_LED_PIN, LOW);
-  }
-
-  if (drive_speed >= 0){
-    analogWrite(FORWARDSPEED_LED_PIN, map(drive_speed, 0, DRIVE_SPEED_MAX, 0, 255));
+#ifdef INDICATOR_LEDS
+  void setupControlIndicators(){
+    pinMode(LEFT_TURN_LED_PIN, OUTPUT);
+    pinMode(RIGHT_TURN_LED_PIN, OUTPUT);
+    pinMode(FORWARDSPEED_LED_PIN, OUTPUT);
     #ifdef BIDIRECTIONAL_DRIVE
-      digitalWrite(BACKWARDSPEED_LED_PIN, LOW);
+      pinMode(BACKWARDSPEED_LED_PIN, OUTPUT);
     #endif
-  }else{
-    digitalWrite(FORWARDSPEED_LED_PIN, LOW);
-    #ifdef BIDIRECTIONAL_DRIVE
-      analogWrite(BACKWARDSPEED_LED_PIN, map(-drive_speed, 0, DRIVE_SPEED_MAX, 0, 255));
-    #endif
+    updateControlIndication();
+  }
+  void updateControlIndication(){
+    if (steer_angle >= 0){
+      analogWrite(RIGHT_TURN_LED_PIN, map(steer_angle, 0, STEERING_ANGLE_MAX, 0, 255));
+      digitalWrite(LEFT_TURN_LED_PIN, LOW);
+    }else{
+      analogWrite(LEFT_TURN_LED_PIN, map(-steer_angle, 0, STEERING_ANGLE_MAX, 0, 255));
+      digitalWrite(RIGHT_TURN_LED_PIN, LOW);
+    }
+  
+    if (drive_speed >= 0){
+      analogWrite(FORWARDSPEED_LED_PIN, map(drive_speed, 0, DRIVE_SPEED_MAX, 0, 255));
+      #ifdef BIDIRECTIONAL_DRIVE
+        digitalWrite(BACKWARDSPEED_LED_PIN, LOW);
+      #endif
+    }else{
+      digitalWrite(FORWARDSPEED_LED_PIN, LOW);
+      #ifdef BIDIRECTIONAL_DRIVE
+        analogWrite(BACKWARDSPEED_LED_PIN, map(-drive_speed, 0, DRIVE_SPEED_MAX, 0, 255));
+      #endif
+    }
+  }
+#endif
+
+bool headlight_on = false;
+void setupHeadlight(){
+  pinMode(HEADLIGHT_PIN, OUTPUT);
+  analogWrite(HEADLIGHT_PIN, HEADLIGHT_OFF>0?HIGH:LOW);
+}
+void turnHeadlightOn(){
+  if (!headlight_on){
+    digitalWrite(HEADLIGHT_PIN, HEADLIGHT_ON>0?HIGH:LOW);
+    headlight_on = true;
   }
 }
-
+void turnHeadlightOff(){
+  if (headlight_on){
+    digitalWrite(HEADLIGHT_PIN, HEADLIGHT_OFF);
+    headlight_on = false;
+  }
+}
+void setHeadlight(uint8_t val){
+  analogWrite(HEADLIGHT_PIN, map(val, 0, 255, HEADLIGHT_OFF, HEADLIGHT_ON));
+}
 
 // COBS encoding, 0 is packet delimiter, 1024-byte buffer size
 PacketSerial_<COBS, 0, 1024> _packet_serial;
@@ -112,7 +136,10 @@ void updateSerial(){
 
 void setup(){
   setupControls();
-  setupControlIndicators();
+  #ifdef INDICATOR_LEDS
+    setupControlIndicators();
+  #endif
+  setupHeadlight();
   setupSerialCommunication();
 }
 
@@ -125,14 +152,20 @@ void loop(){
 
   cur_time = millis();
   if (cur_time - last_debug_msg_time > DEBUG_MSG_PERIOD_MS){
-    Serial.println("S" + String(steer_angle) + "\tD" + String(drive_speed));
+    Serial.println("S" + String(steer_angle) + "\tD" + String(drive_speed) + "\tH" + (headlight_on?"On":"Off"));
     last_debug_msg_time = cur_time;
   }
 
   // If we have a new command, process it
   if (received_commands){
     updateControl();
-    updateControlIndication();
+    
+    turnHeadlightOn();
+    
+    #ifdef INDICATOR_LEDS
+      updateControlIndication();
+    #endif
+    
     received_commands = false;
 
   // If there's no new commands...  
@@ -142,8 +175,11 @@ void loop(){
     // If we go too long without receiving any commands, stop the car
     if (cur_time - last_command_receive_time_ms > SERIAL_UPDATE_TIMOUT_MS){
       drive_speed = 0;
+      turnHeadlightOff();
       updateControl();
-      updateControlIndication();
+      #ifdef INDICATOR_LEDS
+        updateControlIndication();
+      #endif
     }
   }
 }
